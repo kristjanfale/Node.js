25c52aa41b863f3803843cf2d5d3d2cd524a769e
Kristjan Fale <kristjan.fale@gmail.com>
2020-01-07 18:34:05 +0100

    deploy

--- index.js
+++ index.js
@@ -3,30 +3,85 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // If url is '/'
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' }); // status -> 200, content-type -> html
-      res.end(data); // Output data
-    });
+  //   // If url is '/'
+  //   if (req.url === '/') {
+  //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html' }); // status -> 200, content-type -> html
+  //       res.end(data); // Output data
+  //     });
+  //   }
+  //   if (req.url === '/about') {
+  //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html' }); // status -> 200, content-type -> html
+  //       res.end(data); // Output data
+  //     });
+  //   }
+  //   // If we want to fetch data(in this case hardcoded users arry) from '/api/users'
+  //   if (req.url === '/api/users') {
+  //     const users = [
+  //       { name: 'Bob Bobson', age: 30 },
+  //       { name: 'John Green', age: 44 }
+  //     ];
+  //     res.writeHead(200, { 'Content-Type': 'application/json' }); // status -> 200, content-type -> json
+  //     res.end(JSON.stringify(users));
+  //   }
+
+  // Build dinamic file path
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Default content type
+  let contentType = 'text/html';
+  // Chech ext and set content type
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
   }
-  if (req.url === '/about') {
-    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
-      if (err) throw err;
-      res.writeHead(200, { 'Content-Type': 'text/html' }); // status -> 200, content-type -> html
-      res.end(data); // Output data
-    });
-  }
-  // If we want to fetch data(in this case hardcoded users arry) from '/api/users'
-  if (req.url === '/api/users') {
-    const users = [
-      { name: 'Bob Bobson', age: 30 },
-      { name: 'John Green', age: 44 }
-    ];
-    res.writeHead(200, { 'Content-Type': 'application/json' }); // status -> 200, content-type -> json
-    res.end(JSON.stringify(users));
-  }
+
+  // Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // Page not found -> load 404.html
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+          }
+        );
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
